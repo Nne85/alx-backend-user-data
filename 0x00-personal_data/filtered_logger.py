fe944e10b9972c4b8df022923c877963dd3f8996
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-23 19:28:45 +0000

    Logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,14 +50,14 @@ class RedactingFormatter(logging.Formatter):
         return text
 
 def get_logger() -> logging.Logger:
-    """  returns a logging.Logger object. """
-    logger = logging.getLogger('user_data')
+    """Returns a Logger Object"""
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
 
     return logger
 
