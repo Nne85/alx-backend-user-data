293ce53330607a651d6e86faaaabddb8a0e9871f
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-23 04:48:12 +0000

    Redact PII

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Returns Log Message
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -25,6 +26,26 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = rf"(?<={separator})({'|'.join(fields)})=(.*?)(?={separator})"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Obfuscates specified fields in the log message and
+        formats the record."""
+        msg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
 
 
 if __name__ == "__main__":
