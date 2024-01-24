68658dd0e4f9091dfc5935d873ada842a57543af
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-24 22:30:46 +0000

    403 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """ New endpoint for testing 401 error"""
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ New endpoint for testing 403 error"""
+    abort(403)
