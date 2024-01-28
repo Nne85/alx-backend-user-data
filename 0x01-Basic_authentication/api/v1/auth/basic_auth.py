f666058a246db9256972d457b91efe6fff7c9c47
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-28 04:32:09 +0000

    Added Basic Auth Class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 basic_auth module
 """
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -27,3 +29,25 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 string.
+
+        Args:
+        - base64_authorization_header (str): The Base64 string to decode.
+
+        Returns:
+        - str: The decoded value as a UTF-8 string or None.
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except base64.binascii.Error:
+            return None
