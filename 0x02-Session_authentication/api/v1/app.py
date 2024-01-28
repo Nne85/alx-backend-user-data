352a4b805b3041ee2b99f81c4e646e742630cd16
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-01-28 14:53:53 +0000

    Session Authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,9 +20,13 @@ if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if auth_type == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Session class that inherits from Auth Class"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class SessionAuth"""
+    pass
