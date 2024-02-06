98145086f361fab509c0a625aa4eb873694b24a0
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-02-06 19:14:05 +0000

    Create session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,15 @@ class Auth:
             return True
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """creates session using emaill"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
