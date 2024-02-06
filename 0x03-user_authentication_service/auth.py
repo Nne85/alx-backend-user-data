522f164b9242a659ae7519054e7cdcf869b6643f
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-02-06 13:39:41 +0000

    Hash Password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes the input password with salt using bcrypt.hashpw
+
+    Args:
+        password (str): The password to hash
+
+    Returns:
+        bytes: The salted hash of the input password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
